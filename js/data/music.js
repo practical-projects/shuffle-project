1cdb95094d67977038774fdb78ec937d77543f07
Simona <simonasablinaite@gmail.com>
2023-03-12 15:08:41 +0200

    Create songs list

---
+++ js/data/music.js
@@ -0,0 +1,24 @@
+const trackList = [
+   {
+      name: "Man viskas kanarai",
+      path: "../../img/audio/2Kvėpavimas - Man viskas Kanarai.mp3",
+      singer: "2Kvepavimas",
+   },
+   {
+      name: "Without Me",
+      path: "./img/audio/Eminem - Without Me (Official Music Video).mp3",
+      singer: "Eminem",
+   },
+   {
+      name: "Calm Down Long, Tik Tik",
+      path: "./img/audio/Eminem - Without Me (Official Music Video).mp3",
+      singer: "Rema, Selena Gomez",
+   },
+   {
+      name: "Melody",
+      path: "./img/audio/Eminem - Without Me (Official Music Video).mp3",
+      singer: "Sigala",
+   },
+]
+
+export default trackList;
--- js/main-home.js
+++ js/main-home.js
@@ -1,13 +1,19 @@
+import trackList from "./data/music.js";
 // new-album start
 const audio = document.getElementById('audio');
 const playPauseBtn = document.getElementById('playPause');
+const prevSong = document.getElementById('prevSong');
+const nextSong = document.getElementById('nextSong');
+const songName = document.getElementById('songName')
 let count = 0;
 
+// Play and pause song
 playPauseBtn.addEventListener('click', () => {
    if (count == 0) {
       count = 1;
       audio.play();
       playPauseBtn.innerHTML = "&#9208;";
+      songName.innerHTML = "Ran-D & Endymion ft. LePrince - Run From Reality";
    } else {
       count = 0;
       audio.pause();
