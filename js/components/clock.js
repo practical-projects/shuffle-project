c5282d708e15f796857c1de9fc8e33d07f40791f
saulius <sjsaulius@gmail.com>
2023-03-16 22:07:41 +0200

    clock js done

--- js/components/clock.js
+++ js/components/clock.js
@@ -5,6 +5,7 @@ class clock {
 
     this.DOM = null;
     this.allValuesDOM = null;
+
     this.init();
   }
 
@@ -12,66 +13,77 @@ class clock {
     if (!this.isValidSelector()) {
       return false;
     }
+
     this.render();
+    this.updateClock();
   }
 
   isValidSelector() {
     if (typeof this.selector !== "string" || this.selector === "") {
-      console.error("Error: selector has to be non-empty string");
+      console.error("ERROR: selector has to be non-empty string");
       return false;
     }
+
     this.DOM = document.querySelector(this.selector);
     if (!this.DOM) {
       console.error("could not find element");
       return false;
     }
+
     return true;
   }
 
   formatTime(timeValues) {
-    const updateTime = [];
+    const updatedTime = [];
+
     for (let i = 0; i < timeValues.length; i++) {
       const time = timeValues[i];
       if (i === 0 || time > 9) {
-        updateTime.push(time);
+        // originalus tekstas
+        updatedTime.push(time);
       } else {
-        updateTime.push("0" + time);
+        // pridedam 0 prieki
+        updatedTime.push("0" + time);
       }
     }
-    return updateTime;
+
+    return updatedTime;
   }
 
   calcDeadline() {
+    // dabartinis laikas
     const now = new Date();
-    // dabartiniai metai
+    // einamieji metai
     const todayYear = now.getFullYear();
-    // koncerto data
+    // numanoma data
     let concertTime = todayYear + "-" + this.targetDate;
-    // numatomas laikas
+    // numanomas laikas
     let concertDate = new Date(concertTime);
-
+    // dabartines milisekundes
     const nowMiliseconds = now.getTime();
+    // numanomos milisekundes
     let futureMiliseconds = concertDate.getTime();
 
-    if (nowMiliseconds > now) {
+    if (nowMiliseconds > futureMiliseconds) {
       concertTime = todayYear + 1 + "-" + this.targetDate;
       concertDate = new Date(concertTime);
       futureMiliseconds = concertDate.getTime();
     }
-
+    // likusios milisekundes
     const remainingMiliseconds = futureMiliseconds - nowMiliseconds;
+    // likusios sekundes
     let remainingSeconds = Math.floor(remainingMiliseconds / 1000);
 
-    const dienos = Math.floor(remainingSeconds / 60 / 60 / 24);
-    remainingSeconds -= dienos * 60 * 60 * 24;
+    const days = Math.floor(remainingSeconds / 60 / 60 / 24);
+    remainingSeconds -= days * 60 * 60 * 24;
 
-    const valandos = Math.floor(remainingSeconds / 60 / 60);
-    remainingSeconds -= valandos * 60 * 60;
+    const hours = Math.floor(remainingSeconds / 60 / 60);
+    remainingSeconds -= hours * 60 * 60;
 
-    const minutes = remainingSeconds / 60;
+    const minutes = Math.floor(remainingSeconds / 60);
     remainingSeconds -= minutes * 60;
 
-    return [dienos, valandos, minutes, remainingSeconds];
+    return [days, hours, minutes, remainingSeconds];
   }
 
   updateClock() {
@@ -85,33 +97,19 @@ class clock {
 
   render() {
     const timeValues = this.formatTime(this.calcDeadline());
-    const titleValues = ["Days", "Hours", "Minutes", "Seconds"];
+    const labelValues = ["Days", "Hours", "Minutes", "Seconds"];
     let HTML = "";
 
     for (let i = 0; i < timeValues.length; i++) {
       HTML += `<div class="time">
-           <div class="value">${timeValues[i]} </div>
-           <div class="title">${titleValues[i]}</div>
-         </div>`;
+                      <div class="value">${timeValues[i]}</div>
+                      <div class="label">${labelValues[i]}</div>
+                  </div>`;
     }
+
     this.DOM.innerHTML = HTML;
     this.allValuesDOM = this.DOM.querySelectorAll(".value");
   }
 }
-// function clock(selector) {
-//   const DOM = document.querySelector(selector);
-//   const timeValues = [6, 50, 9, 49, 20];
-//   const titleValues = ["Months", "Days", "Hours", "Minutes", "Seconds"];
-//   let HTML = "";
-
-//   for (let i = 0; i < timeValues.length; i++) {
-//     HTML += `<div class="time">
-//         <div class="value">${timeValues[i]} </div>
-//         <div class="title">${titleValues[i]}</div>
-//       </div>`;
-//   }
-
-//   DOM.innerHTML = HTML;
-// }
 
 export { clock };
