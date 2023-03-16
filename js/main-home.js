92a0c18f7839461d4478ab9064ccc822a5f691dc
Simona <simonasablinaite@gmail.com>
2023-03-16 12:17:00 +0200

    Updates

--- js/main-home.js
+++ js/main-home.js
@@ -8,47 +8,45 @@ const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 
-// dainu pavadinimu sarasaras
 const songs = ['2Kvėpavimas feat. Adrina - Švyturys tarp vandenynų', '2Kvėpavimas - Panamera', '2Kvėpavimas - Vasarinė mergaitė', '2Kvėpavimas - Leidžiu tau viską', '2Kvėpavimas - Man viskas Kanarai'];
-
 let songIndex = 0;
 
 loadSong(songs[songIndex]);
 
-// Ikeliamas dainos pavadinimas kaip parametras:
 function loadSong(song) {
    title.innerText = song;
    audio.src = `../img/music/${song}.mp3`;
-};
+}
+
 function playSong() {
    musicContainer.classList.add("play");
    playBtn.querySelector("i.fa-solid").classList.remove("fa-play");
    playBtn.querySelector("i.fa-solid").classList.add("fa-pause");
    audio.play();
 }
+
 function pauseSong() {
    musicContainer.classList.remove("play");
    playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
    playBtn.querySelector("i.fa-solid").classList.add("fa-play");
    audio.pause();
 }
+
 function prevSong() {
    songIndex--;
-   if (songIndex < 0) {
-      songIndex = songs.length - 1;
-   }
+   if (songIndex < 0) songIndex = songs.length - 1;
    loadSong(songs[songIndex]);
    playSong();
 }
+
 function nextSong() {
    songIndex++;
 
-   if (songIndex > songs.length - 1) {
-      songIndex = 0;
-   }
+   if (songIndex > songs.length - 1) songIndex = 0;
    loadSong(songs[songIndex]);
    playSong();
 }
+
 function updateProgress(e) {
    const { duration, currentTime } = e.srcElement;
    const progressPercent = (currentTime / duration) * 100;
@@ -61,18 +59,15 @@ function setProgress(e) {
    audio.currentTime = (clickX / width) * duration;
 }
 
-// event Listeners:
 playBtn.addEventListener('click', () => {
    const isPlaying = musicContainer.classList.contains("play");
    isPlaying ? pauseSong() : playSong();
-});
+})
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
-
 audio.addEventListener('timeupdate', updateProgress);
 progressContainer.addEventListener('click', setProgress);
-
 audio.addEventListener('ended', nextSong);
 
 loadSong(songs[songIndex]);
